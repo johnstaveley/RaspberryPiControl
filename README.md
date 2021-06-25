b29c040be29d6f85c5be7a17d9ac0c6a8f77cfd6
John Staveley <john@staveley.org>
2021-06-25 17:10:11 +0100

    Added control of servo Added folder for solution items

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 This project is to control a 4 relay header board on a Raspberry Pi.
 This involved porting some python code to C#.
+
+Setup
+Stack 4 relay board on top of Raspberry pi
+Servo1: Attach Brown to pin 9, Red to pin 2 and Yellow to pin 12
+
+
+Tech notes
+Publish as single file does not seem to work, you get a file not found error
+You may have to follow the instructions here to run without sudo: https://raspberrypi.stackexchange.com/questions/40105/access-gpio-pins-without-root-no-access-to-dev-mem-try-running-as-root
+sudo apt-get update
+sudo apt-get upgrade
+sudo chown root.gpio /dev/gpiomem
+sudo chmod g+rw /dev/gpiomem
--- Relayport/Program.cs
+++ Relayport/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Threading;
 using Unosquare.RaspberryIO;
+using System.Device.Gpio;
+using System.Device.Pwm.Drivers;
 using Unosquare.WiringPi;
 
 namespace RelayPort
@@ -12,31 +14,46 @@ namespace RelayPort
         {
             Console.WriteLine("Hello Relay!!");
             Pi.Init<BootstrapWiringPi>();
+            var pwmController = new SoftwarePwmChannel(18, 400, 0.5, true);
+            pwmController.Start();
             try
             {
-                //foreach (var device in Pi.I2C.Devices)
-                //{
-                //    Console.WriteLine($"Registered I2C Device: {device.DeviceId}");
-                //}
                 Console.WriteLine("Starting relay");
                 var relay = new FourRelay(0);
+                for (int i = 0; i < 100; i++)
+                {
+                    pwmController.DutyCycle = i / 100D;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < 100; i++)
+                {
+                    pwmController.DutyCycle = (100 - i) /  100D;
+                    Thread.Sleep(100);
+                }
+
                 for (int i = 0; i < 10; i++)
                 {
+                    pwmController.DutyCycle = 0.0;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                     relay.SetRelay(1, 1);
+                    pwmController.DutyCycle = 0.25;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                     relay.SetRelay(2, 1);
+                    pwmController.DutyCycle = 0.5;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                     relay.SetRelay(3, 1);
+                    pwmController.DutyCycle = 0.75;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                     relay.SetRelay(4, 1);
+                    pwmController.DutyCycle = 1.0;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                     relay.SetAll(0);
+                    pwmController.DutyCycle = 0.5;
                     Console.WriteLine($"Values of Relays are 1:{relay.Get(1)} 2:{relay.Get(2)} 3:{relay.Get(3)} 4:{relay.Get(4)}");
                     Thread.Sleep(1000);
                 }
@@ -46,8 +63,9 @@ namespace RelayPort
                 // Do nothing, there is just no device
                 Console.WriteLine("No Device");
             }
-            Console.WriteLine("Relay Finished. Press a key to end");
+            Console.WriteLine("Relay Finished, press key to end");
             Console.ReadKey();
         }
+        
     }
 }
